3e9ad5ce963eff6015383848ca9e05f8427c39c2
veenavikas <veenavikas17@gmail.com>
2026-04-28 16:38:14 +0530

    feat: make hero banner clickable redirecting to products

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -2,12 +2,13 @@
 
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Hero() {
   return (
     <section className="relative w-full h-[100dvh] min-h-[600px] overflow-hidden">
       {/* Background Hero Banner */}
-      <div className="absolute inset-0 z-0">
+      <Link href="/products" className="absolute inset-0 z-0 block cursor-pointer">
         <Image
           src="/images/products/hero banner.png"
           alt="IRA Beverages Hero Banner"
@@ -15,7 +16,7 @@ export default function Hero() {
           className="object-cover"
           priority
         />
-      </div>
+      </Link>
 
       {/* Decorative Gradient Overlay (Optional, for Header Visibility) */}
       <div className="absolute top-0 left-0 right-0 h-32 bg-gradient-to-b from-black/50 to-transparent z-10 pointer-events-none"></div>
