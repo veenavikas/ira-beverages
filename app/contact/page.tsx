ceba7fa1633fa14f6325045f2fb94ec51b5955a0
veenavikas <veenavikas17@gmail.com>
2026-04-28 16:00:52 +0530

    fix: add missing Image import in ContactPage to resolve runtime error

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
+import Image from 'next/image'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
